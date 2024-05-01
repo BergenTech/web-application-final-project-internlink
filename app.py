09dda888640815b92e36c43063203171edde81cb
Leo Langley <leolan25@bergen.org>
2024-05-01 09:50:16 -0400

    5/1

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ import os
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 import csv
+from itsdangerous import URLSafeTimedSerializer
+from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
 login_manager = LoginManager(app)
@@ -16,6 +18,14 @@ login_manager.login_view = 'login'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'Leo'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USERNAME'] = 'emregemici@gmail.com'
+app.config['MAIL_PASSWORD'] = 'cxke ztxi bhac vqim'  
+app.config['MAIL_DEFAULT_SENDER'] = 'emregemici@gmail.com'
+serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'pdf', 'doc', 'docx'}
 if not os.path.exists(UPLOAD_FOLDER):
@@ -23,7 +33,7 @@ if not os.path.exists(UPLOAD_FOLDER):
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 db = SQLAlchemy(app)
 
-class Admin(db.Model):
+class Admin(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     admin_name = db.Column(db.String(100), nullable=False)
     admin_email = db.Column(db.String(100), nullable=False, unique=True)
@@ -48,7 +58,7 @@ class Admin(db.Model):
     def load_user(user_id):
         return Admin.query.get(int(user_id))
 
-class Organization(db.Model):
+class Organization(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     company_name = db.Column(db.String(100), nullable=False)
     website = db.Column(db.String(100), nullable=False)
@@ -60,6 +70,7 @@ class Organization(db.Model):
     email = db.Column(db.String(100), nullable=False)
     internship_mentor = db.Column(db.String(100), nullable=False)
     internship_topic = db.Column(db.String(100), nullable=False)
+    major = db.Column(db.String(100))
 
     def get_id(self):
         return str(self.id)
@@ -80,7 +91,7 @@ class Organization(db.Model):
     def load_user(user_id):
         return Organization.query.get(int(user_id))
     
-class Intern(db.Model):
+class Intern(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     intern_name = db.Column(db.String(100), nullable=False)
     intern_email = db.Column(db.String(100), nullable=False, unique=True)
@@ -88,6 +99,8 @@ class Intern(db.Model):
     intern_password = db.Column(db.String(100), nullable=False)
     major = db.Column(db.String(100))
     resume = db.Column(db.String(100))
+    email_verified = db.Column(db.Boolean, default=False)
+    claimed_job = db.Column(db.Integer, db.ForeignKey('organization.id'))
 
     def get_id(self):
         return str(self.id)
@@ -133,7 +146,8 @@ def import_csv_data():
                         phone=row['PHONE'],
                         email=row['EMAIL'],
                         internship_mentor=row['INTERNSHIP MENTOR'],
-                        internship_topic=row['INTERNSHIP TOPIC']
+                        internship_topic=row['INTERNSHIP TOPIC'],
+                        major=row['MAJORS']
                     )
                     db.session.add(new_organization)
                 db.session.commit()
@@ -150,6 +164,62 @@ def logout():
     logout_user()
     flash('You have been logged out successfully!', 'success')
     return redirect(url_for('index'))
+
+@app.route('/claim_job/<int:organization_id>', methods=['POST'])
+@login_required
+def claim_job(organization_id):
+    if request.method == 'POST':
+        intern_id = current_user.id
+        intern = Intern.query.get(intern_id)
+        if intern:
+            intern.claimed_job = organization_id
+            db.session.commit()
+            flash('You have successfully claimed this job!', 'success')
+        else:
+            flash('Error: Unable to claim job.', 'error')
+    return redirect(url_for('view_jobs'))
+
+@app.route('/delete_profile/<int:intern_id>', methods=['POST'])
+def delete_profile(intern_id):
+    intern = Intern.query.get_or_404(intern_id)
+    db.session.delete(intern)
+    db.session.commit()
+    flash('Profile deleted successfully!', 'success')
+    return redirect(url_for('view_interns'))
+
+@app.route('/delete_organization/<int:organization_id>', methods=['POST'])
+def delete_organization(organization_id):
+    organization = Organization.query.get_or_404(organization_id)
+    db.session.delete(organization)
+    db.session.commit()
+    flash('Organization deleted successfully!', 'success')
+    return redirect(url_for('view_jobs'))
+
+mail = Mail(app)
+
+def send_email(recipient, subject, body):
+    msg = Message(subject, recipients=[recipient])
+    msg.body = body
+    mail.send(msg)
+
+@app.route('/send_message', methods=['POST'])
+@login_required
+def send_message():
+    if request.method == 'POST':
+        from_email = request.form.get('from_email')
+        to_email = request.form.get('to_email')
+        subject = request.form.get('subject')
+        message = request.form.get('message')
+
+        msg = Message(subject, sender=from_email, recipients=[to_email])
+        msg.body = message
+        mail.send(msg)
+
+        flash('Message sent successfully!', 'success')
+        return redirect(url_for('view_interns'))
+    else:
+        flash('Invalid request!', 'error')
+        return redirect(url_for('view_interns'))
 
 @app.route('/contact')
 def contact():
@@ -188,50 +258,46 @@ def register_intern():
 
         hashed_password = generate_password_hash(intern_password)
 
+        existing_intern = Intern.query.filter_by(intern_email=intern_email).first()
+        if existing_intern:
+            flash('An account with this email already exists. Please use a different email.', 'error')
+            return redirect(url_for('register_intern'))
+
         new_intern = Intern(intern_name=intern_name, intern_email=intern_email, graduation_year=graduation_year, intern_password=hashed_password)
         db.session.add(new_intern)
-        db.session.commit()
 
-        flash('Intern registered successfully!', 'success')
+        try:
+            db.session.commit()
+        except IntegrityError as e:
+            db.session.rollback()
+            flash('An error occurred while registering. Please try again.', 'error')
+            return redirect(url_for('register_intern'))
+
+        token = serializer.dumps(intern_email, salt='email-verification')
+        verification_url = url_for('verify_email', token=token, _external=True)
+        message = f'Click the following link to verify your email address: {verification_url}'
+        send_email(intern_email, 'Email Verification', message)
+
+        flash('A verification email has been sent to your email address. Please verify your email to complete registration. CHECK SPAM BOX!', 'success')
         return redirect(url_for('login'))
 
     return render_template('register_intern.html')
 
-@app.route('/add_organization', methods=['GET', 'POST'])
-@login_required
-def add_organization():
-    if request.method == 'POST':
-        company_name = request.form['company_name']
-        website = request.form['website']
-        street_address = request.form['street_address']
-        city = request.form['city']
-        state = request.form['state']
-        zip_code = request.form['zip_code']
-        phone = request.form['phone']
-        email = request.form['email']
-        internship_mentor = request.form['internship_mentor']
-        internship_topic = request.form['internship_topic']
-
-        new_organization = Organization(
-            company_name=company_name,
-            website=website,
-            street_address=street_address,
-            city=city,
-            state=state,
-            zip_code=zip_code,
-            phone=phone,
-            email=email,
-            internship_mentor=internship_mentor,
-            internship_topic=internship_topic
-        )
-
-        db.session.add(new_organization)
-        db.session.commit()
-
-        flash('Organization added successfully!', 'success')
-        return redirect(url_for('add_organization'))
-
-    return render_template('add_organization.html')
+@app.route('/verify_email/<token>')
+def verify_email(token):
+    try:
+        intern_email = serializer.loads(token, salt='email-verification', max_age=3600)
+        intern = Intern.query.filter_by(intern_email=intern_email).first()
+        if intern:
+            intern.email_verified = True
+            db.session.commit()
+            flash('Your email has been verified successfully!', 'success')
+        else:
+            flash('Invalid verification link.', 'error')
+    except:
+        flash('The verification link is invalid or has expired.', 'error')
+    
+    return redirect(url_for('login'))
 
 @app.route('/register/admin', methods=['GET', 'POST'])
 def register_admin():
@@ -251,10 +317,49 @@ def register_admin():
         db.session.add(new_admin)
         db.session.commit()
 
-        flash('Intern registered successfully!', 'success')
+        flash('Admin registered successfully!', 'success')
         return redirect(url_for('login'))
 
     return render_template('register_admin.html')
+
+@app.route('/add_organization', methods=['GET', 'POST'])
+@login_required
+def add_organization():
+    if request.method == 'POST':
+        company_name = request.form['company_name']
+        website = request.form['website']
+        street_address = request.form['street_address']
+        city = request.form['city']
+        state = request.form['state']
+        zip_code = request.form['zip_code']
+        phone = request.form['phone']
+        email = request.form['email']
+        internship_mentor = request.form['internship_mentor']
+        internship_topic = request.form['internship_topic']
+        majors = request.form.getlist('selected_majors')
+        major_string=', '.join(majors)
+
+        new_organization = Organization(
+            company_name=company_name,
+            website=website,
+            street_address=street_address,
+            city=city,
+            state=state,
+            zip_code=zip_code,
+            phone=phone,
+            email=email,
+            internship_mentor=internship_mentor,
+            internship_topic=internship_topic,
+            major=major_string
+        )
+
+        db.session.add(new_organization)
+        db.session.commit()
+
+        flash('Organization added successfully!', 'success')
+        return redirect(url_for('add_organization'))
+
+    return render_template('add_organization.html')
 
 @app.route("/")
 def index():
@@ -277,15 +382,21 @@ def login():
 
         intern_user = Intern.query.filter_by(intern_email=email).first()
         if intern_user and check_password_hash(intern_user.intern_password, password):
-            session['user_id'] = intern_user.id
-            session['user_type'] = 'Intern'
-            flash('Logged in successfully!', 'success')
-            return redirect(url_for('profile_intern'))
-        
+            if intern_user.email_verified:
+                session['user_id'] = intern_user.id
+                session['user_type'] = 'Intern'
+                login_user(intern_user)
+                flash('Logged in successfully!', 'success')
+                return redirect(url_for('profile_intern'))
+            else:
+                flash('Please verify your email address to login.', 'error')
+                return redirect(url_for('login'))
+
         admin_user = Admin.query.filter_by(admin_email=email).first()
         if admin_user and check_password_hash(admin_user.admin_password, password):
             session['user_id'] = admin_user.id
             session['user_type'] = 'Admin'
+            login_user(admin_user)
             flash('Logged in successfully!', 'success')
             return redirect(url_for('profile_admin'))
 
@@ -302,15 +413,17 @@ def view_jobs():
 @app.route("/view_interns")
 def view_interns():
     interns = Intern.query.all()
-    return render_template("view_interns.html", interns=interns)
+    organization = Organization
+    return render_template("view_interns.html", interns=interns, Organization=organization)
 
 @app.route("/profile/intern")
 def profile_intern():
     if session.get('user_id') and session.get('user_type') == 'Intern':
         user_id = session['user_id']
         intern = Intern.query.get(user_id)
+        organization = Organization
         login_user(intern)
-        return render_template("profile.html", user=intern, user_type='Intern')
+        return render_template("profile.html", user=intern, user_type='Intern', Organization=organization)
     else:
         flash('You do not have permission to access this page.', 'error')
         return redirect(url_for('index'))
@@ -340,6 +453,7 @@ def edit_profile():
         user.intern_email = request.form.get('intern_email', user.intern_email)
         user.graduation_year = request.form.get('graduation_year', user.graduation_year)
         user.major = request.form.get('major')
+        user.claimed_job = request.form.get('claimed_job')
         if 'resume' in request.files:
             resume_file = request.files['resume']
             if resume_file and allowed_file(resume_file.filename):
@@ -353,6 +467,39 @@ def edit_profile():
     db.session.commit()
     flash('Profile updated successfully!', 'success')
     return redirect(url_for(f'profile_{user_type.lower()}'))
+
+@app.route('/edit_interns/<int:intern_id>', methods=['GET', 'POST'])
+def edit_interns(intern_id):
+    intern = Intern.query.get(intern_id)
+    if intern is None:
+        flash('Intern not found.', 'error')
+        return redirect(url_for('view_interns'))
+
+    if request.method == 'POST':
+        intern_name = request.form['intern_name']
+        intern_email = request.form['intern_email']
+        graduation_year = request.form['graduation_year']
+        major = request.form['major']
+        resume = request.files['resume']
+
+        intern.intern_name = intern_name
+        intern.intern_email = intern_email
+        intern.graduation_year = graduation_year
+        intern.major = major
+
+        if resume:
+            filename = secure_filename(resume.filename)
+            resume.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            intern.resume = filename
+
+        if 'delete_claimed_job' in request.form:
+            intern.claimed_job = None
+
+        db.session.commit()
+        flash('Intern information updated successfully.', 'success')
+        return redirect(url_for('view_interns'))
+
+    return render_template('view_interns.html', intern=intern)
 
 @app.route('/download_resume/<filename>')
 def download_resume(filename):
